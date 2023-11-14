19cfd79a48d68555f0ba002e1e9801c30ff37b47
emanbarhoma30 <emanahmedb92@gmail.com>
2023-11-14 12:11:32 -0800

    handling_ROT13.c

--- handling_ROT13.c
+++ handling_ROT13.c
@@ -1,40 +1,46 @@
 #include "main.h"
 
 /**
- * handle_ROT - function handle the %R conversion specifier (ROT13).
- *
- * @arguments: Variable arguments list.
- * @buffer: Pointer to the output buffer.
- * @buffer_index: Index for the buffer pointer.
- * Return: The number of characters printed.
- */
+* handle_ROT - function handle the %R conversion specifier (ROT13).
+*
+* @arguments: Variable arguments list.
+* @buffer: Pointer to the output buffer.
+* @buffer_index: Index for the buffer pointer.
+* Return: The number of characters printed.
+*/
+
 int handle_ROT(va_list arguments, char *buffer, unsigned int buffer_index)
 {
-	unsigned int i;
+	unsigned int x, y, z;
+	char default_alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot_alpha[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	char *input_string = va_arg(arguments, char *);
 
 	if (input_string == NULL)
 	{
-		return (buffer_index);
+		const char *default_s = "(avyy)";
+
+		for (x = 0; default_s[x]; x++)
+			buffer_index = handle_buffer(buffer, default_s[x], buffer_index);
+		return (6);
 	}
 
-	for (i = 0; input_string[i] != '\0'; i++)
 
+	for (x = 0; input_string[x] != '\0'; x++)
 	{
-		char current_char = input_string[i];
-
-		if ('A' <= current_char && current_char <= 'Z')
+		for (z = y = 0; default_alpha[y]; y++)
 		{
-			current_char = ((current_char - 'A' + 13) % 26) + 'A';
+			if (input_string[x] == default_alpha[y])
+			{
+				z = 1;
+				buffer_index = handle_buffer(buffer, rot_alpha[y], buffer_index);
+				break;
+			}
 		}
-		else if ('a' <= current_char && current_char <= 'z')
-		{
-			current_char = ((current_char - 'a' + 13) % 26) + 'a';
-		}
-
-		buffer_index = handle_buffer(buffer, current_char, buffer_index);
+		if (z == 0)
+			buffer_index = handle_buffer(buffer, input_string[x], buffer_index);
 	}
 
-	return (buffer_index);
+	return (x);
 }
